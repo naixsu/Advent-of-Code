0239a25132d34b997e721a6a9586e4497d2b45e7
naixsu <plnacua02@gmail.com>
2025-03-09 17:42:49 +0800

    Day 2 part 2

--- 2023/day_2/part_2.py
+++ 2023/day_2/part_2.py
@@ -8,8 +8,28 @@ def read_from_file(file_name: str) -> str:
 def get_sum() -> int:
     file_name = "input.txt"
     lines = read_from_file(file_name).split("\n")
-
+    colors = {
+        "red": 0, "green": 0, "blue": 0,
+    }
     total = 0
+
+    for line in lines:
+        line = line.split(" ")
+
+        for i, char in enumerate(line):
+            char = char.strip(":,;")
+
+            if char in colors:
+                colors[char] = max(
+                    int(line[i - 1]),
+                    colors[char]
+                )
+
+        total += colors["red"] * colors["green"] * colors["blue"]
+
+        colors = {
+            "red": 0, "green": 0, "blue": 0,
+        } 
 
     return total
 
