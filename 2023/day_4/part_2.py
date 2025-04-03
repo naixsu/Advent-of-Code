efb3daf9bc6110bb05e960f726c29f7c937a191a
Paul Lexus Nacua <paulnacua.bposeats@gmail.com>
2025-04-03 16:51:17 +0800

    Day 4 part 1

---
+++ 2023/day_4/part_2.py
@@ -0,0 +1,41 @@
+def read_from_file(file_name: str) -> str:
+    with open(file_name, "r") as file:
+        line = file.read()
+
+    return line
+
+
+def get_sum() -> int:
+    file_name = "input.txt"
+    lines = read_from_file(file_name).split("\n")
+
+    total = 0
+    cards = {}
+
+    for i, line in enumerate(lines):
+        line = line.split("|")
+        winning = line[0]
+        winning = winning.split(":")[1].strip(" ").split(" ")
+        winning = [num for num in winning if num != ""]
+        winning = set(map(int, winning))
+
+        mine = line[1]
+        mine = mine.strip(" ").split(" ")
+        mine = [num for num in mine if num != ""]
+        mine = map(int, mine)
+
+        count = sum(1 for num in mine if num in winning)
+        cards[i + 1] = count
+
+    print(cards)
+
+    return total
+
+
+def main() -> None:
+    sum = get_sum()
+    print(sum)
+
+
+if __name__ == "__main__":
+    main()
