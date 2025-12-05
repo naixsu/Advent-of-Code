31e86f910a05da312aaaf47be8999a8404441c9f
naixsu <plnacua02@gmail.com>
2025-12-05 20:48:25 +0800

    Day 1 part 2

--- 2025/day_1/part_2.py
+++ 2025/day_1/part_2.py
@@ -5,11 +5,35 @@ def read_from_file(file_name: str) -> str:
     return line
 
 
+def get_rotations(lines: list[str]) -> list[tuple[str, int]]:
+    rotations = []
+
+    for line in lines:
+        direction = line[0]
+        dials = int(line[1:])
+
+        rotations.append((direction, dials))
+
+    return rotations
+
+
 def get_sum() -> int:
     file_name = "input.txt"
     lines = read_from_file(file_name).split("\n")
+    rotations = get_rotations(lines)
 
     total = 0
+    start = 50
+    curr = start
+
+    for direction, dials in rotations:
+        for _ in range(dials):
+            if direction == "L":
+                curr = (curr - 1) % 100
+            else:
+                curr = (curr + 1) % 100
+            if curr == 0:
+                total += 1
 
     return total
 
