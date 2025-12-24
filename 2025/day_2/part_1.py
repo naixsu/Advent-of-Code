94919d84d7a191af0293cde27b09dc5185236a75
naixsu <plnacua02@gmail.com>
2025-12-24 16:21:33 +0800

    Day 2 part 1

---
+++ 2025/day_2/part_1.py
@@ -0,0 +1,55 @@
+import math
+
+def read_from_file(file_name: str) -> str:
+    with open(file_name, "r") as file:
+        line = file.read()
+
+    return line
+
+
+def get_ids(lines: list[str]) -> list[str]:
+    # Acc to the problem, we are expecting a single line
+    return lines[0].split(",")
+
+
+def get_number_of_digits(num: int) -> int:
+    return int(math.log10(abs(num))) + 1
+
+
+def is_even_digits(num: int) -> bool:
+    return get_number_of_digits(num) % 2 == 0
+
+
+def is_invalid(num: str) -> bool:
+    length = len(num)
+    mid = length // 2
+
+    return num[0:mid] == num[mid:length]
+
+def get_sum() -> int:
+    file_name = "input.txt"
+    lines = read_from_file(file_name).split("\n")
+    ids = get_ids(lines)
+
+    total = 0
+
+    for id in ids:
+        first, last = id.split("-")
+
+        for num in range(int(first), int(last) + 1):
+            if not is_even_digits(num):
+                continue
+
+            if is_invalid(str(num)):
+                total += num
+
+    return total
+
+
+def main() -> None:
+    sum = get_sum()
+    print(sum)
+
+
+if __name__ == "__main__":
+    main()
